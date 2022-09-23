d4a9e9e696be63e8636b220895d001224d121c81
Ivan Dovhopol <ivandovhopol@gmail.com>
2022-09-24 01:05:40 +0300

    checked

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+// import './css/common.scss';
+
 const qwe = 2 > 1 ? 'yes' : 'no';
 console.log(qwe);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,8 +7,26 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node-modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
   devServer: {
-    port: 3333,
     open: true,
   },
 };
